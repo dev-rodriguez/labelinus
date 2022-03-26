b5a99ed2c1e4a6815b2ca629270e440fe5daa397
Ignacio Rodriguez <ignacio.rodriguez.gacitua@gmail.com>
2022-03-25 22:25:03 -0700

    Added routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -11,12 +12,28 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListProductsComponent } from './components/commerce/product/list-products/list-products.component';
 import { CardProductComponent } from './components/commerce/product/card-product/card-product.component';
+import { ProductDetailsComponent } from './components/commerce/product/product-details/product-details.component';
+import { CartIconComponent } from './components/commerce/cart/cart-icon/cart-icon.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'premade-products', component: ListProductsComponent },
+  { path: 'home', component: HomeComponent },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ListProductsComponent,
-    CardProductComponent
+    CardProductComponent,
+    ProductDetailsComponent,
+    CartIconComponent,
+    ToolbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +43,11 @@ import { CardProductComponent } from './components/commerce/product/card-product
     MatButtonModule,
     MatFormFieldModule,
     IvyCarouselModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
