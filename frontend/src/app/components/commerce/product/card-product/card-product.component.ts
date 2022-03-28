eea74d24fb5360d64098b3c078f427cbcf74b97d
Ignacio Rodriguez <ignacio.rodriguez.gacitua@gmail.com>
2022-03-28 11:06:47 -0700

    Replace commerce service for commerce sdk

--- frontend/src/app/components/commerce/product/card-product/card-product.component.ts
+++ frontend/src/app/components/commerce/product/card-product/card-product.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import { Product } from 'src/app/interfaces/product';
+import { CartService } from 'src/app/services/cart.service';
+import { environment } from 'src/environments/environment';
+import Commerce from '@chec/commerce.js';
 
 @Component({
   selector: 'app-card-product',
@@ -9,9 +13,27 @@ import { Product } from 'src/app/interfaces/product';
 export class CardProductComponent implements OnInit {
 
   @Input() productDetail: Product
-  constructor() { }
+  private cartId: String
+  private commerce
+  constructor(private cart: CartService) { }
 
   ngOnInit(): void {
+    this.commerce = new Commerce(environment.commerceKey);
+  }
+
+  createCart() {
+    this.cart.createCart()
+    .subscribe((data: any) => {
+      console.log('cart created', data)
+      sessionStorage.setItem("cartId", data.id)
+    })
+  }
+
+  async addProduct(product: Product, quantity = 1) {
+    this.cartId = sessionStorage.getItem('cartId')
+    console.log(product)
+    this.commerce.cart.add(product.id, quantity)
+    .then((response) => console.log(response));
   }
 
 }
