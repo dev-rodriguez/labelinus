510f7d92a7c810413023933455e934c9e17ab694
Ignacio Rodriguez <ignacio.rodriguez.gacitua@gmail.com>
2022-03-29 12:30:09 -0700

    Add checkout and update cart

--- frontend/src/app/components/commerce/cart/cart-details/cart-details.component.ts
+++ frontend/src/app/components/commerce/cart/cart-details/cart-details.component.ts
@@ -1,15 +1,30 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  ViewChild
 } from '@angular/core';
 import Commerce from '@chec/commerce.js';
-import { Item } from 'src/app/interfaces/item';
+import {
+  Item
+} from 'src/app/interfaces/item';
 import {
   Product
 } from 'src/app/interfaces/product';
 import {
   environment
 } from 'src/environments/environment';
+import {
+  FormBuilder,
+  FormGroup,
+  FormGroupDirective,
+  Validators
+} from '@angular/forms';
+import {
+  ValidatorService
+} from 'src/app/services/validators/validator.service';
+import {
+  states
+} from 'src/app/interfaces/states';
 
 @Component({
   selector: 'app-cart-details',
@@ -18,13 +33,25 @@ import {
 })
 export class CartDetailsComponent implements OnInit {
 
+  @ViewChild(FormGroupDirective) formDirective: FormGroupDirective;
+
   private commerce
+  private checkoutId
   public spinner = true
-  public items: Item [] = []
+  public items: Item[] = []
   public cart
+  public states = states
 
+  public shippingForm: FormGroup = this.fb.group({
+    addressLine1: ['', [Validators.required]],
+    addressLine2: [''],
+    city: ['', [Validators.required]],
+    state: ['', [Validators.required]],
+    zipCode: ['', [Validators.required]],
+    shippingOption: ['', [Validators.required]]
+  });
 
-  constructor() {}
+  constructor(private fb: FormBuilder, private validatorService: ValidatorService) {}
 
   ngOnInit(): void {
     this.commerce = new Commerce(environment.commerceKey);
@@ -36,9 +63,12 @@ export class CartDetailsComponent implements OnInit {
     this.commerce.cart
       .retrieve()
       .then((cart) => {
+        console.log(cart)
         this.cart = cart
       });
   }
+
+
 
   getIems() {
     this.commerce.cart.contents().then((elements) => {
@@ -62,7 +92,7 @@ export class CartDetailsComponent implements OnInit {
     });
   }
 
-  dropItem (item: Item) {
+  dropItem(item: Item) {
     this.commerce.cart.remove(item.id).then((response) => {
       this.items = []
       this.spinner = true
@@ -71,4 +101,38 @@ export class CartDetailsComponent implements OnInit {
     });
   }
 
+  updateItemQuantity(item: Item, operation) {
+    let quantity = item.quantity
+
+    if (operation === '+') {
+      quantity = quantity + 1
+    }
+    if (operation === '-') {
+      quantity = quantity - 1
+    }
+
+    if (quantity > 0) {
+      this.commerce.cart.update(item.id, {
+          quantity: quantity
+        })
+        .then(response => {
+          console.log(response)
+          this.items = []
+          this.spinner = true
+          this.getCart()
+          this.getIems()
+        });
+    } else {
+      this.dropItem(item)
+    }
+
+
+  }
+
+  notValidField(campo: string) {
+    return (
+      this.shippingForm.get(campo) ?.invalid &&
+      this.shippingForm.get(campo) ?.touched
+    );
+  }
 }
