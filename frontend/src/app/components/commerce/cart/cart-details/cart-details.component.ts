b29f7e57bbb758b11ddbf12f3b20d3de8ce151f8
Ignacio Rodriguez <ignacio.rodriguez.gacitua@gmail.com>
2022-03-28 17:17:46 -0700

    Added the checkout section

--- frontend/src/app/components/commerce/cart/cart-details/cart-details.component.ts
+++ frontend/src/app/components/commerce/cart/cart-details/cart-details.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 import Commerce from '@chec/commerce.js';
-import { Product } from 'src/app/interfaces/product';
-import { environment } from 'src/environments/environment';
+import { Item } from 'src/app/interfaces/item';
+import {
+  Product
+} from 'src/app/interfaces/product';
+import {
+  environment
+} from 'src/environments/environment';
 
 @Component({
   selector: 'app-cart-details',
@@ -9,34 +17,57 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./cart-details.component.scss']
 })
 export class CartDetailsComponent implements OnInit {
-  
+
   private commerce
-  public products: Product [] = []
+  public spinner = true
+  public items: Item [] = []
   public cart
-  
-  constructor() { }
+
+
+  constructor() {}
 
   ngOnInit(): void {
     this.commerce = new Commerce(environment.commerceKey);
-    this.commerce.cart
-    .retrieve()
-    .then((cart) => {
-      console.log(cart)
-      this.cart = cart
-    });
+    this.getCart()
+    this.getIems()
+  }
 
-    this.commerce.cart.contents().then((items) => {
-      console.log(items)
-      for (let i = 0; i < items.length; i++) {
+  getCart() {
+    this.commerce.cart
+      .retrieve()
+      .then((cart) => {
+        this.cart = cart
+      });
+  }
+
+  getIems() {
+    this.commerce.cart.contents().then((elements) => {
+      for (let i = 0; i < elements.length; i++) {
         let product: Product = {
-          id: items[i].id,
-          name: items[i].name,
+          id: elements[i].product_id,
+          name: elements[i].name,
           description: '',
-          price: items[i].line_total.formatted_with_symbol,
-          imageUrl: items[i].image.url
+          price: elements[i].price.formatted_with_symbol,
+          imageUrl: elements[i].image.url
         }
-        this.products.push(product)
+        let item: Item = {
+          id: elements[i].id,
+          product: product,
+          quantity: elements[i].quantity,
+          itemPrice: elements[i].line_total.formatted_with_symbol
+        }
+        this.items.push(item)
       }
+      this.spinner = false
+    });
+  }
+
+  dropItem (item: Item) {
+    this.commerce.cart.remove(item.id).then((response) => {
+      this.items = []
+      this.spinner = true
+      this.getCart()
+      this.getIems()
     });
   }
 
