1c5f6465e437c55c5d7ed9a9f14884881fd196f0
Ignacio Rodriguez <ignacio.rodriguez.gacitua@gmail.com>
2022-03-28 17:18:50 -0700

    Change commerce service for commerce sdk

--- frontend/src/app/components/commerce/product/list-products/list-products.component.ts
+++ frontend/src/app/components/commerce/product/list-products/list-products.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit , Input } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
-import { CommerceService } from 'src/app/services/commerce.service';
-import { CartService } from 'src/app/services/cart.service';
+import Commerce from '@chec/commerce.js';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-list-products',
@@ -11,30 +11,30 @@ import { CartService } from 'src/app/services/cart.service';
 export class ListProductsComponent implements OnInit {
 
   public products: Product[] = []
+  public spinner = true
+  private commerce
 
-  constructor(
-    private commerceService: CommerceService, 
-    private cart: CartService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.commerceService.listProducts()
-    .subscribe((response: any) => {
-      console.log(response)
-      for (let i = 0; i < response.data.length; i++) {
+    this.commerce = new Commerce(environment.commerceKey);
+    this.listProducts()
+  }
+
+  listProducts() {
+    this.commerce.products.list().then((elements) => {
+      console.log(elements)
+      for (let i = 0; i < elements.data.length; i++) {
         let product: Product = {
-          id: '',
-          name: '',
-          description: '',
-          price: '',
-          imageUrl: ''
+          id: elements.data[i].id,
+          name: elements.data[i].name,
+          description: elements.data[i].description,
+          price: elements.data[i].price.formatted_with_symbol,
+          imageUrl: elements.data[i].image.url
         }
-        product.id = response.data[i].id
-        product.name = response.data[i].name
-        product.description = response.data[i].description
-        product.price = response.data[i].price.formatted_with_symbol
-        product.imageUrl = response.data[i].image.url
         this.products.push(product)
       }
+      this.spinner = false
     })
   }
 }
